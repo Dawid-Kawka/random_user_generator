7fc08c168bc23d06387fedc02c21d3a732584106
Dawid Kawka <d.kawka@op.pl>
2022-01-20 14:09:12 +0100

    add data to index.js

--- index.js
+++ index.js
@@ -6,4 +6,21 @@ window.onload = function () {
 
 function dataReady(data) {
     console.log(data);
+
+    var result = data.results[0];
+
+    var fullname = result.name.title + " " + result.name.first + " " + result.name.last;
+    document.getElementById("full-name").innerHTML = fullname;
+
+    var img = result.picture.large;
+    document.getElementById("avatar-img").src = img;
+
+    var username = result.login.username;
+    document.getElementById("username").innerHTML = "@" + username;
+
+    var email = result.email;
+    document.getElementById("email").innerHTML = email;
+
+    var country = result.location.city + " " + result.location.country;
+    document.getElementById("country").innerHTML = country;
 }
